678b6329f7e65a2c6ecc7b4615e160128627baa0
Maido Kaiste <kaistemaido@gmail.com>
2019-05-16 13:56:51 +0300

    Loo Kangelane konstruktor

---
+++ Kangelased/Kangelased/Kangelase andmed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kangelased
+{
+    class Kangelase_andmed
+    {
+        private string _Nimi;
+        private string _superNimi;
+        private string _v6ime;
+
+        public Kangelase_andmed(string Nimi, string superNimi, string v6ime)
+        {
+            _Nimi = Nimi;
+            _superNimi = superNimi;
+            _v6ime = v6ime;
+        }
+
+        public string Nimi { get => _Nimi; set => _Nimi = value; }
+        public string SuperNimi { get => _superNimi; set => _superNimi = value; }
+        public string V6ime { get => _v6ime; set => _v6ime = value; }
+    }
+}
